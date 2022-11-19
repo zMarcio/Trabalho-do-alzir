c73df459d69bb446ca96e6ac4110f190f2634efe
zMarcio <marciopaulolima21@edu.unifor.br>
2022-11-19 12:10:49 -0300

    Codigos

--- QuickSort.js
+++ QuickSort.js
@@ -1,3 +1,22 @@
+function partition(items, left, right) {
+    var pivot = items[Math.floor((right + left) / 2)],
+        i = left,
+        j = right;
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
 function quickSort(items, Direita, Esquerda ) {
     var index;
     if (items.length > 1) {
